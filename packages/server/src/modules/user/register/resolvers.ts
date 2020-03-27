a526577c76fad4874c6216d1ed4b43e6217645fc
yashpk789987 <yashpk789987@gmail.com>
2020-03-27 13:14:15 +0530

    sharing code stuff

--- packages/server/src/modules/user/register/resolvers.ts
+++ packages/server/src/modules/user/register/resolvers.ts
@@ -1,25 +1,10 @@
-import * as yup from "yup";
-
 import { ResolverMap } from "../../../types/graphql-utils";
 import { User } from "../../../entity/User";
 import { formatYupError } from "../../../utils/formatYupError";
-import {
-  duplicateEmail,
-  emailNotLongEnough,
-  invalidEmail
-} from "./errorMessages";
-import { registerPasswordValidation } from "../../../yupSchemas";
+import { duplicateEmail } from "./errorMessages";
+import { userValidationSchema } from "@airbnb/common";
 // import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink";
 // import { sendEmail } from "../../utils/sendEmail";
-
-const schema = yup.object().shape({
-  email: yup
-    .string()
-    .min(3, emailNotLongEnough)
-    .max(255)
-    .email(invalidEmail),
-  password: registerPasswordValidation
-});
 
 export const resolvers: ResolverMap = {
   Mutation: {
@@ -29,7 +14,7 @@ export const resolvers: ResolverMap = {
       // { redis, url }
     ) => {
       try {
-        await schema.validate(args, { abortEarly: false });
+        await userValidationSchema.validate(args, { abortEarly: false });
       } catch (err) {
         return formatYupError(err);
       }
--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Form, Icon, Input, Button } from "antd";
 import { withFormik, FormikErrors, FormikProps } from "formik";
-import * as yup from "yup";
+
+import { userValidationSchema } from "@airbnb/common";
 
 interface FormValues {
   email: string;
@@ -77,26 +78,8 @@ class C extends React.PureComponent<FormikProps<FormValues> & Props> {
   }
 }
 
-const emailNotLongEnough = "email must be at least 3 characters";
-const passwordNotLongEnough = "password must be at least 3 characters";
-const invalidEmail = "email must be a valid email";
-
-const validationSchema = yup.object().shape({
-  email: yup
-    .string()
-    .min(3, emailNotLongEnough)
-    .max(255)
-    .email(invalidEmail)
-    .required(),
-  password: yup
-    .string()
-    .min(3, passwordNotLongEnough)
-    .max(255)
-    .required()
-});
-
 export const RegisterView = withFormik<Props, FormValues>({
-  validationSchema,
+  validationSchema: userValidationSchema,
   validateOnChange: false,
   validateOnBlur: false,
   mapPropsToValues: () => ({ email: "", password: "" }),
