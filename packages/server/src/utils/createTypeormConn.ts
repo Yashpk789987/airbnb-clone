d6eb7cdf68970ac62cfcd8d4f3acc5f9e57f01dd
yashpk789987 <yashpk789987@gmail.com>
2020-03-28 18:39:27 +0530

    deployed on Digital Ocean Using Dokku

--- packages/server/src/utils/createTypeormConn.ts
+++ packages/server/src/utils/createTypeormConn.ts
@@ -1,6 +1,14 @@
 import { getConnectionOptions, createConnection } from "typeorm";
+import { User } from "../entity/User";
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  return createConnection({ ...connectionOptions, name: "default" });
+  return process.env.NODE_ENV === "production"
+    ? createConnection({
+        ...connectionOptions,
+        url: process.env.DATABASE_URL as string,
+        entities: [User],
+        name: "default"
+      } as any)
+    : createConnection({ ...connectionOptions, name: "default" });
 };
