d7bc31a155bc9826c59303c9c645c8c07017d7e5
yashpk789987 <yashpk789987@gmail.com>
2020-03-30 20:40:41 +0530

    Generating Graphql Types(Boring)

--- packages/controller/src/modules/RegisterController/__generated__/RegisterMutation.ts
+++ packages/controller/src/modules/RegisterController/__generated__/RegisterMutation.ts
@@ -1,5 +1,3 @@
-
-
 /* tslint:disable */
 // This file was automatically generated and should not be edited.
 
@@ -20,14 +18,3 @@ export interface RegisterMutationVariables {
   email: string;
   password: string;
 }
-
-/* tslint:disable */
-// This file was automatically generated and should not be edited.
-
-//==============================================================
-// START Enums and Input Objects
-//==============================================================
-
-//==============================================================
-// END Enums and Input Objects
-//==============================================================
