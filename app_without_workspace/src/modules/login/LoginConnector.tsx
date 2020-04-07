08ed8d4c0ff1ac1372c28ddbefa0d163689ece85
yashpk789987 <yashpk789987@gmail.com>
2020-04-07 13:34:46 +0530

    Cookies Work In React Native

--- app_without_workspace/src/modules/login/LoginConnector.tsx
+++ app_without_workspace/src/modules/login/LoginConnector.tsx
@@ -1,17 +1,21 @@
 import * as React from "react";
 import { LoginController } from "@airbnb/controller";
 import { LoginView } from "./ui/LoginView";
-import * as SecureStore from "expo-secure-store";
-import { SID_KEY } from "../shared/constants";
+import { RouteComponentProps } from "react-router-native";
+// import * as SecureStore from "expo-secure-store";
+// import { SID_KEY } from "../shared/constants";
 
-export class LoginConnector extends React.PureComponent {
-  onSessionId = async (sid: string) => {
-    SecureStore.setItemAsync(SID_KEY, sid);
+export class LoginConnector extends React.PureComponent<RouteComponentProps> {
+  // onSessionId = async (sid: string) => {
+  //   SecureStore.setItemAsync(SID_KEY, sid);
+  // };
+  onFinish = () => {
+    this.props.history.push("/me");
   };
   render() {
     return (
-      <LoginController onSessionId={this.onSessionId}>
-        {({ submit }) => <LoginView submit={submit} />}
+      <LoginController>
+        {({ submit }) => <LoginView onFinish={this.onFinish} submit={submit} />}
       </LoginController>
     );
   }
