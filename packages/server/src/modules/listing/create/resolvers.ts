5aae58f89df3693c2a31d19e8f777c8be2af31cb
Yashpk789987 <yashpk789987@gmail.com>
2020-04-11 15:16:49 +0530

    Update Listing Mutation Done Backend

--- packages/server/src/modules/listing/create/resolvers.ts
+++ packages/server/src/modules/listing/create/resolvers.ts
@@ -1,27 +1,6 @@
-import * as shortid from "shortid";
-import { createWriteStream } from "fs";
-
 import { ResolverMap } from "../../../types/graphql-utils";
 import { Listing } from "../../../entity/Listing";
-
-const storeUpload = async (stream: any, mimetype: string): Promise<any> => {
-  const extension = mimetype.split("/")[1];
-  const id = `${shortid.generate()}.${extension}`;
-  const path = `images/${id}`;
-
-  return new Promise((resolve, reject) =>
-    stream
-      .pipe(createWriteStream(path))
-      .on("finish", () => resolve({ id, path }))
-      .on("error", reject)
-  );
-};
-
-const processUpload = async (upload: any) => {
-  const { stream, mimetype } = await upload;
-  const { id } = await storeUpload(stream, mimetype);
-  return id;
-};
+import { processUpload } from "../shared/processUploadFile";
 
 export const resolvers: ResolverMap = {
   Mutation: {
---
+++ packages/server/src/modules/listing/update/resolvers.ts
@@ -0,0 +1,26 @@
+import { ResolverMap } from "../../../types/graphql-utils";
+import { Listing } from "../../../entity/Listing";
+import { processUpload } from "../shared/processUploadFile";
+
+export const resolvers: ResolverMap = {
+  Mutation: {
+    updateListing: async (_, { listingId, input: { picture, ...data } }) => {
+      // const pictureUrl = picture ? await processUpload(picture) : null;
+
+      // 1. user uploads a new picture
+      if (picture) {
+        data.pictureUrl = await processUpload(picture);
+      }
+      // 2. user remove a picture
+      // 3. do nothing
+
+      await Listing.update(
+        { id: listingId },
+        {
+          ...data,
+        }
+      );
+      return true;
+    },
+  },
+};
